25d08ead54af33af0ced1a16384b575d48cf9027
Hao Wang <ge_nah@hotmail.com>
2019-06-24 23:15:58 -0700

    Done.

--- app.rb
+++ app.rb
@@ -5,10 +5,10 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+   
     erb :results
   end
 end
